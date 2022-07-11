eac416df320451c4fd4f054246d8a8e1eebb5e04
Max Rudolph <maxwellr@gmail.com>
2022-07-11 13:45:33 -0700

    fix units issue

--- steam_model.m
+++ steam_model.m
@@ -2,51 +2,53 @@ format long
 clc
 clear
 
-par.delxy = 5*10^(-2); % vertical difference between xbar and ybar in m
-par.sb = 613*10^(-4); % cross-section of bubble trap in m^2
-par.sc = 5.07*10^(-4);% cross-section of (1-inch) column in m^2
-par.sl = 7.92*10^(-4); %cross-section of lateral connector in m^2
-par.L = 7.37*10^(-2); % length of lateral connector in m
+% Set up parameters
+par.delxy = 5e-2; % vertical difference between xbar and ybar in m
+par.sb = 0.0613; % cross-section of bubble trap in m^2
+par.sc = 5.07e-4;% cross-section of (1-inch) column in m^2
+par.sl = 7.92e-4; %cross-section of lateral connector in m^2
+par.L = 7.37e-2; % length of lateral connector in m
 par.g = 9.81;% gravitational acceleration in m*s^-2
 par.rho = 1000; % water density in kg*m^-3
 par.gamma=7/5; % adiabatic exponent for diatomic ideal gas - unitless
-par.H = 37.15*10^(-2); % height of bubble trap in cm
-par.alpha = 5/2;%diatomic ideal gas constant - unitless
-par.Pa0 =1e5; %atmospheric pressure at equilibrium in Bar
+par.H = 37.15e-2; % height of bubble trap in m
+par.alpha = 5/2; %diatomic ideal gas constant - unitless
+par.Pa0 =1e5; %atmospheric pressure at equilibrium in Pa
 %(rho=1)g(ybar-xbar) = Pb0-Pa0 from paper
 fill = 0.5; %fraction of chamber above lateral connector that contains water
 par.xbar = fill.*par.H;
 par.ybar = par.delxy + par.xbar;
 
-
-P=linspace(0.007,2,10000); %selected range of pressures in bar
-vV=zeros(1,length(P));
-hH = zeros(1,length(P));
-dh_dp = zeros(1,length(P)); %enthalpy per bar
-dv_dp = zeros(1,length(P)); %m3 per bar
-Vol_0 = par.sb*(par.H-par.xbar); %initial volume in m^3
-P_0 = par.rho*par.g*(par.delxy)+par.Pa0; %initial pressure in kg*s^-2*m^-1 (Pascal)
+%% Make a lookup table with pre-computed thermodynamic properties.
+P=linspace(0.007,2,10000); %Range of pressure for lookup table (bar)
+vV=zeros(1,length(P));  % vapor volume (m^3/kg)
+hH = zeros(1,length(P));% enthalpy (J/kg)
+dh_dp = zeros(1,length(P)); %dH/dP in (kJ/K/Pa)
+dv_dp = zeros(1,length(P)); %dV/dP in (m^3/Pa)
+Vol_0 = par.sb*(par.H-par.xbar); %initial (equilibrium) volume in m^3
+P_0 = par.rho*par.g*(par.delxy)+par.Pa0; %initial pressure in (Pascal)
 par.m=Vol_0 / XSteam('vV_p', (P_0*10^(-5))); %vapor mass in kg
-s=XSteam('sV_p',(P_0*10^(-5))); %specific entropy kJ*kg^-1*degC^-1
+s=XSteam('sV_p',(P_0*10^(-5)))*1000; %specific entropy J*kg^-1*degC^-1
 for i=1:length(P)
-    vV(i) = XSteam('v_ps', P(i),s); %specific volume in m^3*kg^-1
-    hH(i) = XSteam('h_ps',P(i),s); %enthalpy in kJ*kg^-1
+    vV(i) = XSteam('v_ps',P(i),s/1e3); %specific volume in m^3*kg^-1
+    hH(i) = XSteam('h_ps',P(i),s/1e3)*1000; %enthalpy in J*kg^-1
     delta = 1e-6;%bar
-    dh_dp(i) = (XSteam('h_ps',P(i)+delta,s)-XSteam('h_ps',P(i)-delta,s))/(2*delta)/1e5;
-    dv_dp(i) = (XSteam('v_ps', P(i)+delta,s)-XSteam('v_ps', P(i)-delta,s))/(2*delta)/1e5;
+    dh_dp(i) = (XSteam('h_ps',P(i)+delta,s/1e3)-XSteam('h_ps',P(i)-delta,s/1e3))*1000/(2*delta)/1e5;% enthalpy in mks units, pressure in Pa
+    dv_dp(i) = (XSteam('v_ps', P(i)+delta,s/1e3)-XSteam('v_ps', P(i)-delta,s/1e3))*1000/(2*delta)/1e5;
 end
 
 [~,i] = sort(vV);
 par.Fdhdp = griddedInterpolant(vV(i),dh_dp(i));
 par.FdPdv = griddedInterpolant(vV(i),1./dv_dp(i));
 
-h=0.001; %time step
+h=0.001; %time step (s)
 t=0:h:15; %time period of solution in seconds
 
-x0= 0.001*1e-2; %initial displacement in cm
-v0= 0; %initial velocity in cm*s^-1
+x0= -0.15*1e-2; %initial displacement in m
+v0= 0; %initial velocity in m/s
 
-assert(x0<(par.H-par.xbar)) %sense check s.t. water cannot overflow
+assert(x0<(par.H-par.xbar)) %check that water cannot overflow
+assert( (par.ybar - par.sb/par.sc*abs(x0)) > 0.0) % assert that water stays above lateral connector on conduit side.
 
 %initialize x and v list
 x= zeros(1,length(t));
@@ -72,11 +74,17 @@ for i=1:length(t)-1
     x(i+1) = x(i) + (h/6)*(k1x+2*k2x+2*k3x+k4x);
     v(i+1) = v(i) + (h/6)*(k1v+2*k2v+2*k3v+k4v);
 end
-
+%%
 figure;
 plot(t,x);
 title('Solution x(t) of IVP')
 xlabel('t'); grid on
+
+figure();
+y = par.ybar - par.sb/par.sc*x;
+plot(t,y);
+title('Solution y(t)');
+
 
 figure;
 plot(x,v);
