8d265ac6dd8aa72536017d9f98fa351f21af44de
dasuthkarsh <udas@ucdavis.edu>
2022-08-02 12:02:54 -0700

    add plot of frequency vs (y-x)
    compares numerical and analytical frequencies, computes % error

--- steam_model.m
+++ steam_model.m
@@ -1,9 +1,13 @@
-format long;clc;clear;addpath ./XSteam_Matlab_v2.6/;
+format long;
+%clc;clear;
+addpath ./XSteam_Matlab_v2.6/;
 P=linspace(0.007,2,5000); %Range of pressure for lookup table (bar)
 vV=zeros(1,length(P));  % vapor volume (m^3/kg)
 hH = zeros(1,length(P));% enthalpy (J/kg)
 dh_dp = zeros(1,length(P)); %dH/dP in (kJ/K/Pa)
 dv_dp = zeros(1,length(P)); %dV/dP in (m^3/Pa)
+nmfreq = zeros(1,5);
+anfreq = zeros(1,5);
 par.sb = 0.0613; % cross-section of bubble trap in m^2
 par.sc = 5.07e-4;% cross-section of (1-inch) column in m^2
 par.sl = 7.92e-4; %cross-section of lateral connector in m^2
@@ -15,10 +19,10 @@ par.H = 37.15e-2; % height of bubble trap in m
 par.alpha = 5/2; %diatomic ideal gas constant - unitless
 par.Pa0 =1e5; %atmospheric pressure at equilibrium in Pa
 %fill = 0.9; %fraction of chamber above lateral connector that contains water
-par.xbar = 30e-2; %mean water height in bubble trap in m, must be in (0,H)
-par.ybar=15e-2; %mean water height in conduit in m, must be greater than Sb/Sc*x0
-par.delxy = par.ybar-par.xbar;
-
+par.xbar = 25*1e-2; %mean water height in bubble trap in m, must be in (0,H)
+delxys = linspace(-10,10,10)*1e-2;
+ybars = delxys+par.xbar; %mean water height in conduit in m, must be greater than Sb/Sc*x0
+%par.delxy = par.ybar-par.xbar;
 numPar.x0= 0.0005*1e-2; %initial displacement in m
 numPar.v0= 0; %initial velocity in m/s
 numPar.h=0.001; %time step (s)
@@ -26,6 +30,10 @@ numPar.tf = 3;
 numPar.time=0: numPar.h : numPar.tf;
 numPar.j=length(numPar.time);
 
+for k = 1:length(delxys)
+    %par.xbar = xbars(k);
+    par.ybar = ybars(k);
+    par.delxy = delxys(k);
 %% Make a lookup table with pre-computed thermodynamic properties.
 par.Vol_0 = par.sb*(par.H-par.xbar); %initial (equilibrium) volume in m^3
 P_0 = par.rho*par.g*(par.delxy)+par.Pa0; %initial pressure in (Pascal)
@@ -45,10 +53,8 @@ par.FdPdv = griddedInterpolant(vV(i),1./dv_dp(i));
 
 [x,v] = steam_solve(par,numPar);
 %%Plotting
-%{
-b = linspace(5e-2,37.15e-2,100);
-figure; plot(b,par.Fdhdp((1/par.m).*par.sb.*(par.H-par.xbar-b)));title('dhdp vs x')
 
+%{
 t = numPar.time;
 figure;
 plot(t,x.*1e2);
@@ -71,7 +77,7 @@ ylabel('Y (cm)')
 %%compute v(t)
 
 
-figure; plot(b,par.FdPdv((1/par.m).*par.sb.*(par.H-par.xbar-b)));title('dpdv vs x')
+
 vt = par.sb*(par.H - par.xbar - x)/par.m;
 pt = interp1(vV,P,vt);
 xt = zeros(size(pt));
@@ -80,13 +86,21 @@ for i = 1:length(pt)
 end
 figure();
 plot(t,xt);
+
+b = linspace(5e-2,37.15e-2,100);
+figure; plot(b,par.FdPdv((1/par.m).*par.sb.*(par.H-par.xbar-b)));title('dpdv vs x')
+figure; plot(b,par.Fdhdp((1/par.m).*par.sb.*(par.H-par.xbar-b)));title('dhdp vs x')
 %}
 
 [~,locs1] = findpeaks(x);
 T=(locs1(2)-locs1(1))*numPar.h;
-nmfreq = 1/T;
-fprintf('The numerical solution oscillates at a frequency of %f. \n',nmfreq);
+nmfreq(k) = 1/T;
+%fprintf('The numerical solution oscillates at a frequency of %f. \n',nmfreq(k));
 
 
-anfreq = frequency_calculator(par);
-fprintf('The analytical frequency is %f. \n', anfreq);
+anfreq(k) = frequency_calculator(par);
+%fprintf('The analytical frequency is %f. \n', anfreq(k));
+end
+figure;plot(delxys*1e2, nmfreq,delxys*1e2,anfreq);
+err = abs((anfreq-nmfreq)./nmfreq)*100;
+figure;plot(delxys*1e2,err);
