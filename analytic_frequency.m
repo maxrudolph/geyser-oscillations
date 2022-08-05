6124f03ec22c91619f35f24d6d84350f6a9d203b
Max Rudolph <maxrudolph@ucdavis.edu>
2022-08-05 14:22:19 -0700

    add analytic calculation

---
+++ analytic_frequency.m
@@ -0,0 +1,38 @@
+function freq = analytic_frequency(par)
+sb_sc= par.sb ./ par.sc;
+sb_sl= par.sb ./ par.sl;
+
+% define f(x) = dH/dP * -V*dP/dV
+% f(0)/(H-xbar) + (f'(0)/(H-xbar) + f(0)/(H-xbar)^2)*x
+tmp1 = par.Fdhdp(par.Vol_0/par.m) * -par.Vol_0*par.FdPdv(par.Vol_0/par.m);
+delta = 1e-9;
+fplus  = par.Fdhdp((par.Vol_0-par.sb*delta)/par.m) * -(par.Vol_0-par.sb*delta)*par.FdPdv((par.Vol_0-par.sb*delta)/par.m);
+fminus = par.Fdhdp((par.Vol_0+par.sb*delta)/par.m) * -(par.Vol_0+par.sb*delta)*par.FdPdv((par.Vol_0+par.sb*delta)/par.m);
+dfdx = (fplus-fminus)/(2*delta);
+tmp2 = dfdx/(par.H-par.xbar) - tmp1/(par.H-par.xbar)^2;
+
+% k0 = -par.Vol_0.*par.FdPdv(par.Vol_0/par.m);
+
+% xi0 = par.Fdhdp(par.Vol_0/par.m);
+% delta = 1e-3;
+% dxidx0 = (par.Fdhdp((1/par.m)*par.sb*(par.H-par.xbar-delta))-par.Fdhdp((1/par.m)*par.sb*(par.H-par.xbar+delta) )) /(2*delta);
+%dkdx = dp/dvdx * v + dp/dv * dv/dx
+% dpdvdx0v0  = -par.Vol_0*(par.FdPdv((1/par.m)*par.sb*(par.H-par.xbar-delta))-par.FdPdv((1/par.m)*par.sb*(par.H-par.xbar+delta) ) ) /(2*delta);
+% dpdv0dvdx0 = -par.FdPdv(par.Vol_0/par.m)*par.sb;
+% dkdx0 = -(dpdvdx0v0+dpdv0dvdx0);
+Hx = par.H-par.xbar;
+A = par.xbar+sb_sc*par.ybar+sb_sl*par.L;
+B = par.g.*(1+sb_sc);
+C = -1/(par.rho*par.sb);
+% D=(-xi0*k0)/(Hx^2);
+% E=(dxidx0*k0)/Hx;
+% G =(-xi0*dkdx0)/Hx;
+% F=C*(D+E+G);
+F = C*tmp2;
+
+freq = sqrt((B+F)/A)/(2*pi);
+
+
+
+end
+
