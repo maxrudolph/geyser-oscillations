2411246d30b3191ed267afdd67fe2939cfc443b6
dasuthkarsh <udas@ucdavis.edu>
2022-08-31 00:29:11 -0700

    Changed Enthalpy to Internal Energy
    Dependence on specific volume changed to total volume

--- Steam/frequency_calculator.m
+++ Steam/frequency_calculator.m
@@ -1,14 +1,14 @@
 function freq = frequency_calculator(par)
 sbsc= par.sb ./ par.sc;
 sbsl= par.sb ./ par.sl;
-k0 = -par.Vol_0.*par.FdPdv(par.Vol_0/par.m);
+k0 = -par.Vol_0.*par.FdPdv(par.Vol_0);
 
-xi0 = par.Fdhdp(par.Vol_0/par.m);
+xi0 = par.Fdudp(par.Vol_0);
 delta = 1e-3;
-dxidx0 = (par.Fdhdp((1/par.m)*par.sb*(par.H-par.xbar-delta))-par.Fdhdp((1/par.m)*par.sb*(par.H-par.xbar+delta) )) /(2*delta);
+dxidx0 = (par.Fdudp(par.sb*(par.H-par.xbar-delta))-par.Fdudp(par.sb*(par.H-par.xbar+delta) )) /(2*delta);
 %dkdx = dp/dvdx * v + dp/dv * dv/dx
-dpdvdx0v0  = -par.Vol_0*(par.FdPdv((1/par.m)*par.sb*(par.H-par.xbar-delta))-par.FdPdv((1/par.m)*par.sb*(par.H-par.xbar+delta) ) ) /(2*delta);
-dpdv0dvdx0 = -par.FdPdv(par.Vol_0/par.m)*par.sb;
+dpdvdx0v0  = -par.Vol_0*(par.FdPdv(par.sb*(par.H-par.xbar-delta))-par.FdPdv(par.sb*(par.H-par.xbar+delta) ) ) /(2*delta);
+dpdv0dvdx0 = -par.FdPdv(par.Vol_0)*par.sb;
 dkdx0 = -(dpdvdx0v0+dpdv0dvdx0);
 Hx = par.H-par.xbar;
 A = par.xbar+sbsc*par.ybar+sbsl*par.L;
