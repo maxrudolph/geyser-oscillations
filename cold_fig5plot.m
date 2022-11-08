10ddf2c2500666f34646e668a357bd13f56060fc
dasuthkarsh <udas@ucdavis.edu>
2022-11-08 08:45:35 -0800

    use 1 and 2-inch pipe data
    introduced correction factor to account for potential systematic error in xbar

--- cold_fig5plot.m
+++ cold_fig5plot.m
@@ -5,19 +5,33 @@ close all;
 addpath XSteam_Matlab_v2.6\;
 addpath Ideal_Gas\;
 addpath Steam\
-opts = detectImportOptions('cold-water-freq-filtered.csv');
+filename='cold-water-freq-2in-filtered.csv';
+opts = detectImportOptions(filename);
 opts.SelectedVariableNames = [1,2,8,14];
-expdata = readmatrix('cold-water-freq-filtered.csv',opts);
+%opts.DataLines = 29;
+expdata = readmatrix(filename,opts);
 
 fprintf('Using new lab dimensions. \n')
-par.sb = 1928e-4;
-par.sc = 5.07e-4; % cross-section of (1-inch) column in m^2
+par.sb = 1852e-4;
 par.sl = 1; %cross-section of lateral connector in m^2
 par.L = 0; % length of lateral connector
 par.H = 74.7e-2;
-xbars = linspace(0, 74.25,101)*1e-2;
-exp_xbar = expdata(:,1)*1e-2;
+xbars = linspace(0, 74.25,length(expdata))*1e-2;
+correction = 2.5e-2;
+if strcmp(filename,'cold-water-freq-2in-filtered.csv')
+    par.sc = 20.25802e-4; % cross-section of (2-inch) column in m^2
+    exp_xbar = expdata(1:8,1)*1e-2;
+    exp_xbar(9:length(expdata))=expdata(9:end,1)*1e-2 -correction;
+    freqscale=2;
+elseif strcmp(filename,'cold-water-freq-filtered.csv')
+    par.sc = 5.07e-4; % cross-section of (1-inch) column in m^2
+    exp_xbar = expdata(1:27,1)*1e-2;
+    exp_xbar(28:length(expdata))=expdata(28:end,1)*1e-2-correction;
+    freqscale=1;
+end
 exp_ybar = expdata(:,2)*1e-2+par.H;
+exp_err = expdata(:,4)*2*pi;
+exp_frequencies = expdata(:,3)/(2*pi);
 delxys = exp_ybar-exp_xbar;
 par.g = 9.81;% gravitational acceleration in m*s^-2
 par.rho = 1000; % water density in kg*m^-3
@@ -34,15 +48,17 @@ labfrequencies = zeros(length(delxys), length(exp_xbar));
 propagated_errors=zeros(length(delxys), length(exp_xbar));
 for j =1:length(delxys)
     par.delxy = delxys(j);
-    fprintf("Using %f \n", par.delxy*1e2);
     for k = 1:length(xbars)+length(exp_xbar)
         if k<= length(xbars)
             par.xbar = xbars(k);
+            par.ybar = par.delxy + par.xbar;
         elseif k>length(xbars)
             par.xbar = exp_xbar(k-length(xbars));
             par.delxy = delxys((k-length(xbars)));
+            par.ybar = exp_ybar(k-length(xbars));
         end
-        par.ybar = par.delxy + par.xbar;
+        
+        fprintf("Using y-x= %f \n y= %f \n x= %f \n", par.delxy*1e2, par.ybar*1e2, par.xbar*1e2);
         par.Pb0= par.rho*par.g*par.delxy +par.Pa0;
         if k<= length(xbars)
             numfrequencies(j,k) = coldfreq(par);
@@ -52,34 +68,35 @@ for j =1:length(delxys)
         end
     end
 end
-exp_err = expdata(:,4)*2*pi;
-figure;
-exp_frequencies = expdata(:,3)/(2*pi);
-plot(xbars*1e2,numfrequencies, 'MarkerSize',15); hold on
-for l=1:length(exp_frequencies)
-    errorbar(exp_xbar(l)*1e2,exp_frequencies(l),propagated_errors(l,l), '.', 'MarkerSize', 15);
-end
-hold off;
-title('Predicted Oscillation Frequencies, Cold Water')
-ylabel('Frequency (Hz)')
-xlabel('xbar (cm)')
-legend('y-x=40.2 cm','y-x=61.3 cm','y-x=35.1 cm','y-x=36.7 cm','y-x=27.2 cm','y-x=49.7 cm','y-x=17.7 cm','Location','northeast')
-ylim([-0.1,1])
-xlim([0,par.H*1e2])
+% figure;
+% plot(xbars*1e2,numfrequencies, 'MarkerSize',15); hold on
+% for l=1:length(exp_frequencies)
+%     errorbar(exp_xbar(l)*1e2,exp_frequencies(l),propagated_errors(l,l), '.', 'MarkerSize', 15);
+% end
+% hold off;
+% title('Predicted Oscillation Frequencies, Cold Water')
+% ylabel('Frequency (Hz)')
+% xlabel('xbar (cm)')
+% legend('y-x=40.2 cm','y-x=61.3 cm','y-x=35.1 cm','y-x=36.7 cm','y-x=27.2 cm','y-x=49.7 cm','y-x=17.7 cm','Location','northeast')
+% ylim([-0.1,1])
+% xlim([0,par.H*1e2])
 
+%% plot of predicted vs. observed frequencies
 figure;
 p2=errorbar(labfrequencies(1,:), exp_frequencies,exp_err,exp_err,propagated_errors(1,:),propagated_errors(1,:),'.'); hold on
 p2(1).MarkerSize = 15;
-delxyrange=ceil(max(delxys*1e2)) + 1;
-cmap =delxyrange;
-pos_station = [0 0];
-c = sqrt((labfrequencies(1,:)-pos_station(1)).^2 + (exp_frequencies-pos_station(2)).^2);
-c = (c-min(c))/(max(c)-min(c))*delxyrange;  % map to 360 colors
-scatter(labfrequencies(1,:),exp_frequencies,[],c, 'filled');
-colormap(jet(cmap));
+
+cmap = colormap('jet'); % retrieve the jet colormap
+% map delxy onto colors
+color_variable = exp_xbar;
+colors = interp1(linspace(min(color_variable),max(color_variable),length(cmap)),cmap,color_variable);
+caxis([min(color_variable) max(color_variable)])
+
+scatter(labfrequencies(1,:),exp_frequencies,[],colors, 'filled');
 a=colorbar();
-ylabel(a, '$\overline y$ - $\overline x$ (cm)','fontsize',14,'interpreter','latex', 'Rotation',90)
-xave=linspace(0.4,0.7,51); plot(xave,xave,'--');
+ylabel(a, '$\bar x$ (cm)','fontsize',14,'interpreter','latex', 'Rotation',90)
+
+xave=linspace(0.4,.7*freqscale,51); plot(xave,xave,'--');
 xlabel('Predicted Frequency (Hz)')
 ylabel('Observed Frequency (Hz)')
 title('Predicted vs Observed Frequency, Cold Water')
