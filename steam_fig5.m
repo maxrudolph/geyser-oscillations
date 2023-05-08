5fc26b940deaa29c88c6cb81f3d6d5397643ffcb
dasuthkarsh <udas@ucdavis.edu>
2023-05-08 00:16:45 -0700

    Fixed xbar expectations, new error propagation

--- steam_fig5.m
+++ steam_fig5.m
@@ -6,6 +6,19 @@ addpath XSteam_Matlab_v2.6\;
 addpath Ideal_Gas\;
 addpath Steam\
 geometry = 1;
+conduit_diameter = 1; % 1 -> 1 inch, 2 -> 2 inches
+switch conduit_diameter
+    case 1
+        filename='hot-water-freq-filtered-1in_short.csv';
+        par.sc = pi*(1/2*2.54/100)^2; %cross-section of 1-inch tube in cm^2
+        par.H = 31.8e-2; % "new" short 1 inch pickup in m
+        %par.H = 58e-2; % old, longer 1 inch pickup in m
+    case 2
+        filename='hot-water-freq-filtered-2in_v3.csv';
+        par.sc = pi*(2/2*2.54/100)^2; %cross-section of 2-inch tube in cm^2
+        par.H = (53-1.8)*1e-2; %Bubble trap height in m
+end
+
 switch geometry
     case 0
         fprintf('Using old lab dimensions. \n');
@@ -17,41 +30,31 @@ switch geometry
     case 1
         fprintf('Using new lab dimensions. \n')
         par.sb = 1852e-4;
-        par.sc = 5.07e-4; % cross-section of (1-inch) column in m^2
         par.sl = 1; %cross-section of lateral connector in m^2
         par.L = 0; % length of lateral connector
-        par.H = 74.7e-2;
 end
-conduit_diameter = 1; % 1 -> 1 inch, 2 -> 2 inches
-if conduit_diameter == 1
-    filename='hot-water-freq-filtered-1in.csv';
-elseif conduit_diameter ==2
-    filename='hot-water-freq-2in-filtered.csv';
-end
+
 opts = detectImportOptions(filename);
-opts.SelectedVariableNames = [1,2,5,7];
+opts.SelectedVariableNames = [1,2,5,7,8];
 expdata = readmatrix(filename,opts);
 
-xbars = linspace(0, 74.25,101)*1e-2;
-%labxbars = [38,51,51,51,60,66]*1e-2;
+xbars = linspace(0, par.H,101)*1e-2;
 labxbars = (expdata(:,1))*1e-2;
 labybars = (expdata(:,2))*1e-2;
+labsensors = expdata(:,5);
 delxys = labybars-labxbars;
-%labybars = [3.5,37.6,11.4,13,12.5,41]*1e-2 + par.H;
-%delxys = [40.2,61.3,35.1,36.7,27.2,49.7]*1e-2;
+
+
 par.g = 9.81;% gravitational acceleration in m*s^-2
 par.rho = 1000; % water density in kg*m^-3
 par.gamma=7/5; % adiabatic exponent for diatomic ideal gas - unitless
 par.alpha = 5/2; %diatomic ideal gas constant - unitless
 par.Pa0 =1e5; %atmospheric pressure at equilibrium in Pa
-%ybars = delxys+par.xbar; %mean water height in conduit in m, must be greater than Sb/Sc*x0
-numPar.x0 = 1e-4;
-numPar.v0= 0; %initial velocity in m/s
-numPar.tf = 5;
+
 delta = 1e-6;%bar
-numfrequencies = zeros(length(delxys),length(xbars));
-labfrequencies = zeros(length(delxys), length(labxbars));
-propagated_errors=zeros(length(delxys), length(labxbars));
+numfrequencies = zeros(1,length(xbars));
+labfrequencies = zeros(1, length(labxbars));
+propagated_errors=zeros(1, length(labxbars));
 for j =1:length(delxys)
     par.delxy = delxys(j);
     for k = 1:length(xbars)+length(labxbars)
@@ -65,15 +68,14 @@ for j =1:length(delxys)
         %% Make a lookup table with pre-computed thermodynamic properties.
         par.Vol_0 = par.sb*(par.H-par.xbar); %initial (equilibrium) volume in m^3
         P_0 = par.rho*par.g*(par.delxy)+par.Pa0; %initial pressure in (Pascal)
-        par.m=par.Vol_0 / XSteam('vV_p', P_0/1e5); %vapor mass in kg
 
         xv = 1 - 1e-2;
         sv=XSteam('sV_p',P_0/1e5)*1e3; %specific entropy J*kg^-1*degC^-1
         sl=XSteam('sL_p',P_0/1e5)*1e3; %specific entropy J*kg^-1*degC^-1
         s = xv*sv + (1-xv)*sl; % gives specific entropy of water+vapor mixture in J/kg/C
         V0 = XSteam('V_ps',P_0/1e5,s/1e3); % volume per unit mass
-        par.m = par.Vol_0 / V0;
-
+        par.m = par.Vol_0 / V0; %vapor mass in kg
+        
         P=linspace((P_0-9e4)/1e5,(P_0+9e4)/1e5,51); %Range of pressure for lookup table (bar)
         vV=zeros(1,length(P));  % vapor volume (m^3)
         du_dp = zeros(1,length(P)); %dU/dP in (kJ/K/Pa)
@@ -87,44 +89,43 @@ for j =1:length(delxys)
         par.Fdudp = griddedInterpolant(vV(i),du_dp(i),'linear','none');
         par.FdPdv = griddedInterpolant(vV(i),1./dv_dp(i),'linear','none');
         if k<= length(xbars)
-            numfrequencies(j,k) = frequency_calculator(par);
+            numfrequencies(1,k) = frequency_calculator(par);
         elseif k>length(xbars)
-            labfrequencies(j,k-length(xbars))=frequency_calculator(par);
-            propagated_errors(j,k-length(xbars)) = propagation(par,1);
+            [labfrequencies(1,k-length(xbars)), propagated_errors(1,k-length(xbars)) ]=frequency_calculator(par);
+            %propagated_errors(j,k-length(xbars)) = propagation(par,1);
         end
     end
-    fprintf('Using %d out of 36 \n',j);
+    fprintf('Using %d out of %d \n',j, length(delxys));
 end
-%err = [0.0003075117811,0.0006765997287,0.0003365434388,0.000366547911,0.0005258494879,0.0004347155603]*2*pi;
-err = expdata(:,4)/2;
-figure;
+err = expdata(:,4);
 exp_frequencies = expdata(:,3)/(2*pi);
-%exp_frequencies = [3.82,3.19,3.69,3.64,3.75,3.27]/(2*pi);
-plot(xbars*1e2,numfrequencies, 'MarkerSize',15); hold on
-for l=1:6
-    errorbar(labxbars(l)*1e2,exp_frequencies(l),propagated_errors(l,l), '.', 'MarkerSize', 15);
-end
-hold off;
-title('Predicted Oscillation Frequencies')
-ylabel('Frequency (Hz)')
-xlabel('xbar (cm)')
-%legend('y-x=40.2 cm','y-x=61.3 cm','y-x=35.1 cm','y-x=36.7 cm','y-x=27.2 cm','y-x=49.7 cm','y-x=17.7 cm','Location','northeast')
-ylim([-0.1,2])
-xlim([0,par.H*1e2])
+
+% figure;
+% plot(xbars*1e2,numfrequencies, 'MarkerSize',15); hold on
+% for l=1:6
+%     errorbar(labxbars(l)*1e2,exp_frequencies(l),propagated_errors(l,l), '.', 'MarkerSize', 15);
+% end
+% hold off;
+% title('Predicted Oscillation Frequencies')
+% ylabel('Frequency (Hz)')
+% xlabel('xbar (cm)')
+% %legend('y-x=40.2 cm','y-x=61.3 cm','y-x=35.1 cm','y-x=36.7 cm','y-x=27.2 cm','y-x=49.7 cm','y-x=17.7 cm','Location','northeast')
+% ylim([-0.1,2])
+% xlim([0,par.H*1e2])
 
 figure;
 p2=errorbar(labfrequencies(1,:), exp_frequencies,err,err,propagated_errors(1,:),propagated_errors(1,:),'.'); hold on
 p2(1).MarkerSize = 15;
 cmap = colormap('jet'); % retrieve the jet colormap
 % map delxy onto colors
-color_variable = delxys*1e2;
+color_variable = (delxys*1e2);
 colors = interp1(linspace(min(color_variable),max(color_variable),length(cmap)),cmap,color_variable);
 caxis([min(color_variable) max(color_variable)])
 
 scatter(labfrequencies(1,:),exp_frequencies,[],colors, 'filled');
 a=colorbar();
-ylabel(a, '$\bar y - \bar x$ (cm)','fontsize',14,'interpreter','latex', 'Rotation',90)
-xave=linspace(0.45,0.7,51); plot(xave,xave,'--');
+ylabel(a, 'ybar-xbar','fontsize',14,'interpreter','latex', 'Rotation',90)
+xave=linspace(min(labfrequencies)-0.2,max(labfrequencies)+0.2,5); plot(xave,xave,'--');
 xlabel('Predicted Frequency (Hz)','fontsize',14,'interpreter','latex')
 ylabel('Observed Frequency (Hz)','fontsize',14,'interpreter','latex')
 title('Predicted vs Observed Frequency','fontsize',14,'interpreter','latex')
